2fe76ff2c8c5911e04b5cbbea9494034c3dfaafb
CORP\fi185007 <Fred.Iannon@ncr.com>
2019-01-28 15:44:56 -0600

    Gittestbed1 - Class3

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace GitTestbed
     {
         static void Main()
         {
-            Console.WriteLine(@"This change was in the initial commit...commit 2");
+            Console.WriteLine(@"This change was in the initial commit...commit 3");
 
             Console.WriteLine(@"Press Enter to continue");
             Console.ReadLine();
