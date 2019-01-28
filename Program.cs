1fa443d756d9b56d0e324be4472b8bf50660c561
CORP\fi185007 <Fred.Iannon@ncr.com>
2019-01-28 12:46:34 -0600

    GitTestbed1 - Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GitTestbed
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(@"This change was in the initial commit");
+
+            Console.WriteLine(@"Press Enter to continue");
+            Console.ReadLine();
+        }
+    }
+}
